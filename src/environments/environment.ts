bfdb273e3ed250bf7174580bc9f165a98e80ed0e
Carlos Ceballo <carlos96cacv@gmail.com>
2025-12-24 07:17:54 -0400

    fix: update API URL to point to the correct backend for development and production environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: "http://localhost:3000/api/v1/",
-  //api_url: "https://zuenio-backend.vercel.app/api/v1/",
+  //api_url: "http://localhost:3000/api/v1/",
+  api_url: "https://guork-backend.vercel.app/api/v1/",
 };
